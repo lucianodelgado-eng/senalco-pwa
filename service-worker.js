c3eb29c5e87a5671883aa4337bb6cc3d27667b60
lucianodelgado-eng <luciano.delgado@senalco.com.ar>
2026-02-06 20:01:21 -0300

    a

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,5 @@
-const CACHE_NAME = "senalco-pwa-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./estilo.css",
-  "./manifest.json",
-  "./javas.js",
-  "./images/icon-192.png",
-  "./images/icon-512.png",
-  "./images/logo_blanco.jpg",
-  "./images/logo.jpg"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
-  );
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", () => self.clients.claim());
+self.addEventListener("fetch", (e) => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
