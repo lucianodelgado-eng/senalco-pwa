4dc5d7d9492701b8249d3cdbb353ce736b9158f8
lucianodelgado-eng <luciano.delgado@senalco.com.ar>
2026-02-06 15:56:55 -0300

    add

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,20 @@
-const CACHE_NAME = "senalco-pwa-v2";
+const CACHE = "senalco-v1";
 
-const ASSETS = [
+const FILES = [
   "./",
-  "./login.html",
-  "./login.js",
+  "./index.html",
   "./manifest.json",
   "./estilo.css",
-
-  "./relevamiento.html",
-  "./relevamiento.js",
-  "./base.html",
-  "./base.js",
-
-  "./images/logo_blanco.jpg",
+  "./login.js",
+  "./images/logo.png",
   "./images/icon-192.png",
   "./images/icon-512.png"
 ];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(FILES)));
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
-  );
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
 });
